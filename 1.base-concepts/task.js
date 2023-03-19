23db09d736c65561f62c109efb59901f34df73c8
Sheremet1 <eparis@atlassian.com>
2023-03-19 22:22:40 +0300

    initil commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,48 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  let oneRoot = -b / (2 * a);
+  let firstRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
+  let secondRoot = (-b - Math.sqrt(discriminant)) / (2 * a);
+  if (discriminant < 0) {
+    arr = [];
+  } else if (discriminant === 0) {
+    arr.push(oneRoot);
+  } else if (discriminant > 0) {
+    arr.push(firstRoot, secondRoot);
+  }
   return arr;
 }
-
+solveEquation;
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let mortgage;
+  if (
+    Number.isNaN(percent) === true ||
+    typeof percent != "number" ||
+    percent < 0
+  ) {
+    return false;
+  } else if (
+    Number.isNaN(contribution) === true ||
+    typeof contribution != "number"
+  ) {
+    return false;
+  } else if (Number.isNaN(amount) === true || typeof amount != "number") {
+    return false;
+  } else if (
+    Number.isNaN(countMonths) === true ||
+    typeof countMonths != "number"
+  ) {
+    return false;
+  } else {
+    let bodyCredit = Number(amount) - Number(contribution);
+    let monthPercent = percent / 12 / 100;
+    let monthPayment =
+      bodyCredit *
+      (monthPercent +
+        monthPercent / (Math.pow(1 + monthPercent, countMonths) - 1));
+    mortgage = Number((monthPayment * countMonths).toFixed(2));
+  }
+  return mortgage;
 }
